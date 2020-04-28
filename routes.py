86099d67ea81d67e44bc6b20af93db95ff4bd927
RnScully <emunseelie@gmail.com>
2020-04-27 21:51:12 -0500

    heeey, its a dropdown! 9000% more dropdown than we had yesteray

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from trade_sorts import get_data
 
 app=Flask(__name__)
@@ -6,13 +6,19 @@ app=Flask(__name__)
 path_markets = 'market_commodities.ini' # paths to the data we need
 distances_path = 'dump.csv'
 
+# declare a bunch of variables
 distances, bases, comm_set, base_names = get_data(path_markets, distances_path)
 bases_string = ' '.join([base[0] for base in base_names])
 base_names_list = [base[0] for base in base_names]
+sys = "name does not exit"
+
+
+
+
 @app.route('/')
 def dropdown():
 
-    return render_template('base_dropdown.html', drops = bases_string)
+    return render_template('base_dropdown.html', drops = base_names_list)
 
 if __name__ == '__main__':
     app.run()
